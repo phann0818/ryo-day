b8ca18c65770905485d33a6696b64b4453fe97f9
phann0818 <152852189+phann0818@users.noreply.github.com>
2025-07-05 17:39:33 +0700

    Delete idol-birthday-website 2 directory

--- idol-birthday-website 2/app/api/years/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server"
-import { GoogleSheetsService } from "@/lib/google-sheets"
-
-export async function GET() {
-  const years = await GoogleSheetsService.getAllYears()
-  return NextResponse.json({ years })
-}
